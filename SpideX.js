42d28ab0f4788c52c31de999b0807c8e25fbed42
tylergoetz <goetztyler813@gmail.com>
2016-09-01 09:03:34 -0500

    Prototyping Oscillator
    added range sliders and play/pause functionality prototyped oscillator

--- SpideX.js
+++ SpideX.js
@@ -14,31 +14,48 @@ function closeNav() {
 //WEBAUDIO API
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)(); //define the audio context
 var oscillator = audioCtx.createOscillator(); //creates a simple oscillator for playback
-var gainNode = audioCtx.createGain();         //allow control of the volume of the audiocontext
+var masterGainNode = audioCtx.createGain();         //allow control of the volume of the audiocontext
+var oscGainNode = audioCtx.createGain();
 
 //connect the audio sources together for output
-oscillator.connect(gainNode); //link osc to gainnode
-gainNode.connect(audioCtx.destination); //link gainnode to audiocontext.destination the generic output
+oscillator.connect(oscGainNode); //link osc to gainnode
+oscGainNode.connect(masterGainNode);
+masterGainNode.connect(audioCtx.destination); //link gainnode to audiocontext.destination the generic output
 
+/*Workflow so far:
+  Given a request for a new track---
+    +Create given track type (osc, audiostream, etc)
+    +Create controller nodes for track (allows for individual mute, volume control)
+    +if track is midi controllabe i.e. vitrual instrument
+      -create input-ready keyboard, allows for scheduled playback of the given input
+*/
 
+//Work in progress of prototyping basic oscillator track
 oscillator.type = 'sine';
-oscillator.frequency.value = 500;
-var oscPlaying = false;
-oscillator.start();
+oscillator.frequency.value = 60;
+var oscPlaying = false
+var oscInit = false;
 
 function toggleSound(){
+  if(!oscInit){
+    oscillator.start();
+    oscInit = true;
+  }
   if(oscPlaying == true){
-    gainNode.disconnect(audioCtx.destination);
+    oscGainNode.disconnect(masterGainNode);
     oscPlaying = false;
   }
   else{
-    gainNode.connect(audioCtx.destination);
+    oscGainNode.connect(masterGainNode);
     oscPlaying = true;
   }
 }
 
 function toggleRange(){
   oscillator.frequency.value = oscRange.value;
+}
+function toggleVolume(){
+  oscGainNode.gain.value = oscVolume.value;
 }
 
 //have to wait for the page to fully load before searching for DOM elements
@@ -51,6 +68,12 @@ var oscRange = document.getElementById("oscRange");
 oscRange.addEventListener("click", toggleRange, false);
 oscRange.min = 60;
 oscRange.max = 1000;
+
+var oscVolume = document.getElementById("oscVolume");
+oscVolume.addEventListener("click", toggleVolume, false);
+oscVolume.min = 0;
+oscVolume.max = 2;
+oscVolume.step = 0.1;
 }
 
 //oscillator.stop(audioCtx.currentTime + 2);
